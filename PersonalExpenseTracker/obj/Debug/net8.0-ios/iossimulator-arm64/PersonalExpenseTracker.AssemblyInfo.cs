cab3fb48a0b51c4cb8a9291efd73072e33f69b27
zsyzygy <doc.adityashah@gmail.com>
2024-12-18 09:04:58 +0545

    initial

--- PersonalExpenseTracker/obj/Debug/net8.0-ios/iossimulator-arm64/PersonalExpenseTracker.AssemblyInfo.cs
+++ PersonalExpenseTracker/obj/Debug/net8.0-ios/iossimulator-arm64/PersonalExpenseTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a72eb81563992de17ae92ea1aebbfdb3e14925ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
